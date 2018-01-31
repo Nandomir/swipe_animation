41e693b13a8d3b9ffad8de94f1e6bac395ab4960
Nandomir <f.sendyka@gmail.com>
2018-01-31 01:53:06 +0000

    Created Deck.js & renderCard function in App.js

---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Animeted} from 'react-native';
+
+class Deck extends Component {
+  render () {
+    return (
+      <View />
+      );
+  }
+}
+
+export default Deck;
