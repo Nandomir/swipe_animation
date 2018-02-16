7138ea0dd1773a13da836e86dfd55e3f9263b22f
Nandomir <f.sendyka@gmail.com>
2018-02-16 00:07:09 +0000

    Added gesture setValues to onPanResponderMove in order to move all deck of cards at once

--- src/Deck.js
+++ src/Deck.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
-import { View, Animated } from 'react-native';
+import { 
+  View, 
+  Animated,
+  PanResponder,
+   } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({  // PanResponder is a self contained object
+      onStartShouldSetPanResponder: () => true, // whenever user taps on the screen PanResponder handles the gesture and this function is called
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx , y: gesture.dy })
+      },  // whenever user drags anything around the screen; 'gesture' argument holds info about the user movements - most important argument
+      onPanResponderRelease: () => {} // user presses the screen and lets the card go
+    });
+
+    this.state = { panResponder, position }; // following the PanResponder docs, even though it's never being called
+  }
+
   renderCards() {
     return this.props.data.map(item=> {
       return this.props.renderCard(item);
     });
   }
 
-  render () {
+  render() {
     return (
-      <View>
+      <Animated.View 
+      style={this.state.position.getLayout()}
+      {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
+      // panHandlers has callbacks that intercept presses from the user - ... is spreading the properties over the view. An exampe of connecting panResponder to an element. 
       );
   }
 }
