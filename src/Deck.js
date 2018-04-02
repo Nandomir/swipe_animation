657e5bcf8659230980f307c2cce526c21395157e
Nandomir <f.sendyka@gmail.com>
2018-04-02 17:28:39 +0100

    renderCards now returns Animated.View instead of View component in order to get rid of flashing images when loading a new card.

--- src/Deck.js
+++ src/Deck.js
@@ -101,9 +101,9 @@ class Deck extends Component {
       }
 
       return (
-        <View key={item.id} style={styles.cardStyle}>
+        <Animated.View key={item.id} style={styles.cardStyle}>
         {this.props.renderCard(item)}
-        </View>
+        </Animated.View>
         );
     }).reverse(); // reverses the .map array from line 88
   }
